a242763dd16184f7029167c6069a36cf30136b6b
jnnkrdb <jannek.rodenburger@icloud.com>
2023-06-19 14:21:03 +0200

    changed flags

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
-	"go.uber.org/zap/zapcore"
+
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
@@ -45,8 +45,6 @@ var (
 	metricsAddr          string
 	enableLeaderElection bool
 	probeAddr            string
-	verbosity            int
-	startDevelopment     bool
 )
 
 func init() {
@@ -60,13 +58,10 @@ func main() {
 
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.IntVar(&verbosity, "verbosity", 0, "Sets the log verbosity level, which produce more logs at a higher number.")
-	flag.BoolVar(&startDevelopment, "mode-development", false, "Adds development output to the logs.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false, "Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
 
 	opts := zap.Options{
-		Development: startDevelopment,
-		Level:       zapcore.Level(verbosity),
+		Development: false,
 	}
 
 	opts.BindFlags(flag.CommandLine)
