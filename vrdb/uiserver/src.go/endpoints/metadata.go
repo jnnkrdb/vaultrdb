9508364febf172b026ed529136893779c6971854
jnnkrdb <jannek.rodenburger@icloud.com>
2024-05-16 00:00:44 +0200

    integrated default functions for the ui frontend server and proxy servings for the storage api

--- vrdb/uiserver/src.go/endpoints/metadata.go
+++ vrdb/uiserver/src.go/endpoints/metadata.go
@@ -1,1 +1,75 @@
 package endpoints
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"vrdb-uiserver/server"
+
+	"github.com/gorilla/mux"
+	"vrdb.go/logging"
+)
+
+const (
+	VERSION_FILE string = "/opt/vaultrdb/config/VERSION"
+	LICENSE_FILE string = "/opt/vaultrdb/config/LICENSE"
+)
+
+const ENABLE_METADATA bool = true
+
+// enables the endpoint for metadata
+//
+// route - http://<host>:<port>/metadata
+func EnableEndpoint_Metadata(r *mux.Router) {
+
+	// if metadata shouldn't be enabled, then skip the insertion of the endpoint
+	if !ENABLE_METADATA {
+		return
+	}
+
+	logging.Log.V(3).Info("creating metadata endpoint under relative path [/metadata]")
+
+	// enable the metadata endpoint for meta informations
+	r.Methods(http.MethodGet).Path("/metadata").Handler(server.DefaultMiddleware.ThenFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+
+			// create the pseudo struct
+			logging.Log.V(5).Info("creating pseudo struct for meta info")
+
+			var result = struct {
+				Version struct {
+					UIServer string `json:"uiserver"`
+				} `json:"version"`
+				License string `json:"license"`
+			}{}
+
+			var (
+				b   []byte
+				err error
+			)
+
+			if b, err = os.ReadFile(VERSION_FILE); err != nil {
+				logging.Log.V(2).Error(err, "error reading bytes from file", "file", VERSION_FILE)
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
+
+			// setting the version for the ui server
+			result.Version.UIServer = string(b)
+
+			// get the license content of the project
+			if b, err = os.ReadFile(LICENSE_FILE); err != nil {
+				logging.Log.V(2).Error(err, "error reading bytes from file", "file", VERSION_FILE)
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
+
+			// setting the license for the project
+			result.License = string(b)
+
+			// parse the result object into json and ship
+			if err := json.NewEncoder(w).Encode(result); err != nil {
+				logging.Log.V(1).Error(err, "error parsing result into json", "result", result)
+			}
+		}))
+}
--- vrdb/uiserver/src.go/main.go
+++ vrdb/uiserver/src.go/main.go
@@ -2,54 +2,26 @@ package main
 
 import (
 	"flag"
-	"net/http"
 
+	"vrdb-uiserver/endpoints"
 	"vrdb-uiserver/server"
 
 	"vrdb.go/logging"
 )
 
-// config via args
-var (
-	ENABLE_SWAGGER bool = false
-	ENABLE_LICENSE bool = true
-	ENABLE_VERSION bool = true
-	ENABLE_UI      bool = true
-	ENABLE_HEALTHZ bool = true
-)
-
 func main() {
 
-	flag.BoolVar(&server.ENABLE_SWAGGER, "swagger", false, "Enables the swagger ui.")
+	flag.BoolVar(&endpoints.ENABLE_SWAGGER, "swagger", false, "Enables the swagger ui.")
+	flag.StringVar(&endpoints.STORAGEAPISERVER, "storageapi-address", "localhost:8080", "Sets the address of the storage api, without the scheme. Port only if neccessary.")
 	flag.Parse()
 
 	logging.InitLogger("ui-server")
 
-	server.StartFrontendUI()
-
-	// append the ui endpoints, which are configured using the args
-	switch {
-
-	case _ENABLE_VERSION: // enable the version endpoint for the ui
-		router.Handle("/version", _mw.ThenFunc(func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "/opt/vaultrdb/config/VERSION") }))
-		log.V(2).Info("enabled version ui", "relative-path", "http://localhost/version")
-
-	case _ENABLE_LICENSE: // enable the license endpoint for the ui
-		router.Handle("/license", _mw.ThenFunc(func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "/opt/vaultrdb/config/LICENSE") }))
-		log.V(2).Info("enabled license ui", "relative-path", "http://localhost/license")
-
-	case _ENABLE_SWAGGER: // enable the swagger endpoint for the ui
-		router.PathPrefix("/swagger/").Handler(_mw.Then(http.StripPrefix("/swagger/", http.FileServer(http.Dir("/opt/vaultrdb/swagger")))))
-		log.V(2).Info("enabled swagger ui", "relative-path", "http://localhost/swagger")
-
-	case _ENABLE_UI: // enable the ui endpoint for the ui
-		router.PathPrefix("/ui/").Handler(_mw.Then(http.StripPrefix("/ui/", http.FileServer(http.Dir("/opt/vaultrdb/web")))))
-		log.V(2).Info("enabled ui", "relative-path", "http://localhost/ui/...")
-
-	case _ENABLE_HEALTHZ: // enable the healthz endpoint for kubernetes
-		router.Handle("/healthz/live", _mw.ThenFunc(func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("OK")) }))
-		router.Handle("/healthz/ready", _mw.ThenFunc(func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("OK")) }))
-		log.V(2).Info("enabled healthz endpoint for kubernetes", "relative-path--liveness", "http://localhost/healthz/live", "relative-path--readiness", "http://localhost/healthz/ready")
-	}
-
+	server.StartFrontendUI(
+		endpoints.EnableEndpoint_Metadata,
+		endpoints.EnableEndpoint_SwaggerUI,
+		endpoints.EnableEndpoint_ApiProxy,
+		endpoints.EnableEndpoint_UI,
+		endpoints.EnableEndpoint_Healthz,
+	)
 }
