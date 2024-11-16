ad0ec1e0e31cdfd692a360e87a609d10f9eff723
jnnkrdb <jannek.rodenburger@icloud.com>
2024-11-16 20:18:36 +0000

    removed old build.bat, not required anymore

--- build.bat
+++
@@ -1,7 +0,0 @@
-rem Build dockerfiles
-cd base
-docker build . --tag vaultrdbbase:latest
-cd ../vrdb
-docker build .  -f Operator.Dockerfile --tag operator:latest
-docker build .  -f StorageAPI.Dockerfile --tag storageapi:latest
-docker build .  -f UiServer.Dockerfile --tag uiserver:latest
