b6fa78383634405a44c8e47740333808e327191c
jnnkrdb <jannek.rodenburger@icloud.com>
2025-06-30 11:54:03 +0000

    added function bodies to the bbolt storage backend

--- pkg/storage/bbolt/bbolt.go
+++ pkg/storage/bbolt/bbolt.go
@@ -1,6 +1,8 @@
 package bbolt
 
 import (
+	"fmt"
+
 	"go.etcd.io/bbolt"
 )
 
@@ -22,34 +24,61 @@ func NewBBoltDB(path string, options *bbolt.Options) (BBoltDB, error) {
 // -------------------------------------------------------------- required functions
 
 func (bb BBoltDB) ListBuckets() ([]string, error) {
-
-	// create the transaction
-	var (
-		result []string = nil
-		err    error    = nil
-	)
-
+	var result []string = nil
+	var err = bb.db.View(func(tx *bbolt.Tx) error {
+		return tx.ForEach(func(name []byte, b *bbolt.Bucket) error {
+			result = append(result, string(name))
+			return nil
+		})
+	})
 	return result, err
 }
 
 func (bb BBoltDB) DeleteBucket(bucket string) error {
-	return nil
+	return bb.db.Update(func(tx *bbolt.Tx) error {
+		return tx.DeleteBucket([]byte(bucket))
+	})
 }
 
 func (bb BBoltDB) ListKeys(bucket string) ([]string, error) {
-
-	// create the transaction
-	var (
-		result []string = nil
-		err    error    = nil
-	)
-
+	var result []string = nil
+	var err = bb.db.View(func(tx *bbolt.Tx) error {
+		return tx.Bucket([]byte(bucket)).ForEach(func(k, v []byte) error {
+			result = append(result, string(k))
+			return nil
+		})
+	})
 	return result, err
 }
 
-func (bb BBoltDB) GetKey(bucket, key string) (string, error)
-func (bb BBoltDB) Write(bucket, key, value string) error
-func (bb BBoltDB) DeleteKey(bucket, key string) error
+func (bb BBoltDB) GetKey(bucket, key string) (string, error) {
+	var result string = ""
+	var err = bb.db.View(func(tx *bbolt.Tx) error {
+		if v := tx.Bucket([]byte(bucket)).Get([]byte(key)); v != nil {
+			result = string(v)
+			return nil
+		} else {
+			return fmt.Errorf("the requested key does not exist or is empty")
+		}
+	})
+	return result, err
+}
+
+func (bb BBoltDB) Write(bucket, key, value string) error {
+	return bb.db.Update(func(tx *bbolt.Tx) error {
+		if b, err := tx.CreateBucketIfNotExists([]byte(bucket)); err != nil {
+			return err
+		} else {
+			return b.Put([]byte(key), []byte(value))
+		}
+	})
+}
+
+func (bb BBoltDB) DeleteKey(bucket, key string) error {
+	return bb.db.Update(func(tx *bbolt.Tx) error {
+		return tx.Bucket([]byte(bucket)).Delete([]byte(key))
+	})
+}
 
 func (bb BBoltDB) Close() error {
 	return bb.db.Close()
